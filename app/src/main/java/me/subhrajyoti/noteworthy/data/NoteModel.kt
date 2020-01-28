27ae10e951a4bf4b99c41e4f58226a0f194ac30d
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2020-01-28 10:40:28 +0530

    Add a unique id for each note

--- app/src/main/java/me/subhrajyoti/noteworthy/data/NoteModel.kt
+++ app/src/main/java/me/subhrajyoti/noteworthy/data/NoteModel.kt
@@ -1,6 +1,9 @@
 package me.subhrajyoti.noteworthy.data
 
+import java.util.*
+
 data class NoteModel(
+    val id: String = UUID.randomUUID().toString(),
     val dateOfCreation: Long,
     val title: String,
     val content: String
