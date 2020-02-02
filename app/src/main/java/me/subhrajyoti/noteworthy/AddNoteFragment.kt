0fd54be05becb7f28ba6f6ae84d0e5c1e2b91144
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2020-02-03 00:32:42 +0530

    Implement saving a note

--- app/src/main/java/me/subhrajyoti/noteworthy/AddNoteFragment.kt
+++ app/src/main/java/me/subhrajyoti/noteworthy/AddNoteFragment.kt
@@ -1,5 +1,43 @@
 package me.subhrajyoti.noteworthy
 
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.fragment_add_note.*
+import me.subhrajyoti.noteworthy.data.NoteModel
+import me.subhrajyoti.noteworthy.utils.NoteValidator
+
 
 class AddNoteFragment : CircularRevealFragment(R.layout.fragment_add_note) {
+
+    private lateinit var notesViewModel: NotesViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        notesViewModel = ViewModelProvider(
+            this,
+            (requireActivity() as MainActivity).viewModelFactory
+        ).get(NotesViewModel::class.java)
+
+        save_note_fab.setOnClickListener {
+            val noteTitle = note_title_editText.text?.toString()
+            if (NoteValidator.isValidText(noteTitle)) {
+                note_title_textInputLayout.isErrorEnabled = false
+
+                val noteContent = note_content_editText.text.toString()
+                val note = NoteModel(
+                    dateOfCreation = System.currentTimeMillis(),
+                    title = noteTitle!!,
+                    content = noteContent
+                )
+                notesViewModel.addNote(note)
+                requireActivity().onBackPressed()
+            } else {
+                note_title_textInputLayout.isErrorEnabled = true
+                note_title_textInputLayout.error =
+                    resources.getString(R.string.empty_note_title_error_message)
+            }
+        }
+    }
 }
