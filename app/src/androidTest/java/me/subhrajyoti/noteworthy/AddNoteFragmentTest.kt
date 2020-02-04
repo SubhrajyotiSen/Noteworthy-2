72a99a7383f641db63ee36d62fd3ef07e561e3e7
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2020-02-04 21:12:29 +0530

    Add a UI test to verify that user text is retained across Fragment recreation

--- app/src/androidTest/java/me/subhrajyoti/noteworthy/AddNoteFragmentTest.kt
+++ app/src/androidTest/java/me/subhrajyoti/noteworthy/AddNoteFragmentTest.kt
@@ -3,10 +3,9 @@ package me.subhrajyoti.noteworthy
 import android.view.View
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.android.material.textfield.TextInputLayout
 import org.hamcrest.Description
@@ -24,6 +23,21 @@ class AddNoteFragmentTest {
         onView(withId(R.id.note_title_editText)).perform(typeText(""))
         onView(withId(R.id.save_note_fab)).perform(click())
         onView(withId(R.id.note_title_textInputLayout)).check(matches(hasTextInputLayoutErrorText("Note title cannot be empty")))
+    }
+
+    /**
+     * The text entered by the user should be retained if the screen is rotated
+     */
+    @Test
+    fun testEnteredTextIsRetained() {
+        val noteTitle = "New note"
+        val noteDescription = "I can't think of any text"
+        val fragment = launchFragmentInContainer<AddNoteFragment>(themeResId = R.style.AppTheme)
+        onView(withId(R.id.note_title_editText)).perform(typeText(noteTitle), closeSoftKeyboard())
+        onView(withId(R.id.note_content_editText)).perform(typeText(noteDescription))
+        fragment.recreate()
+        onView(withId(R.id.note_title_editText)).check(matches(withText(noteTitle)))
+        onView(withId(R.id.note_content_editText)).check(matches(withText(noteDescription)))
     }
 
     private fun hasTextInputLayoutErrorText(expectedErrorText: String): Matcher<View> = object : TypeSafeMatcher<View>() {
