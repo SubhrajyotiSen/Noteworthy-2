017f57352441585de185c6e1d3cf3b12d8220613
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2020-02-06 11:07:48 +0530

    Validate note on keyboard actions

--- app/src/main/java/me/subhrajyoti/noteworthy/AddNoteFragment.kt
+++ app/src/main/java/me/subhrajyoti/noteworthy/AddNoteFragment.kt
@@ -2,6 +2,7 @@ package me.subhrajyoti.noteworthy
 
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.fragment_add_note.*
 import me.subhrajyoti.noteworthy.data.NoteModel
@@ -28,24 +29,52 @@ class AddNoteFragment : CircularRevealFragment(R.layout.fragment_add_note) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        note_title_editText.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_NEXT) {
+                val noteTitle = note_title_editText.text?.toString()
+                validateNoteTitle(noteTitle).not()
+            } else {
+                true
+            }
+        }
+
         save_note_fab.setOnClickListener {
             val noteTitle = note_title_editText.text?.toString()
-            if (NoteValidator.isValidText(noteTitle)) {
-                note_title_textInputLayout.isErrorEnabled = false
+            val noteContent = note_content_editText.text?.toString()
 
-                val noteContent = note_content_editText.text.toString()
+            if (validateNoteTitle(noteTitle) && validateNoteContent(noteContent)) {
                 val note = NoteModel(
                     dateOfCreation = System.currentTimeMillis(),
                     title = noteTitle!!,
-                    content = noteContent
+                    content = noteContent!!
                 )
                 notesViewModel.addNote(note)
                 requireActivity().onBackPressed()
-            } else {
-                note_title_textInputLayout.isErrorEnabled = true
-                note_title_textInputLayout.error =
-                    resources.getString(R.string.empty_note_title_error_message)
             }
+        }
+    }
+
+    private fun validateNoteTitle(noteTitle: String?): Boolean {
+        if (NoteValidator.isValidText(noteTitle)) {
+            note_title_textInputLayout.isErrorEnabled = false
+            return true
+        } else {
+            note_title_textInputLayout.isErrorEnabled = true
+            note_title_textInputLayout.error =
+                resources.getString(R.string.empty_note_title_error_message)
+            return false
+        }
+    }
+
+    private fun validateNoteContent(noteContent: String?): Boolean {
+        if (NoteValidator.isValidText(noteContent)) {
+            note_content_textInputLayout.isErrorEnabled = false
+            return true
+        } else {
+            note_content_textInputLayout.isErrorEnabled = true
+            note_content_textInputLayout.error =
+                resources.getString(R.string.empty_note_title_error_message)
+            return false
         }
     }
 }
