c3b8f462a31f68c5a81f15428d270510ccf16598
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2020-02-06 12:38:39 +0530

    Suppress Unchecked cast warning
    * In our case, the cast will always succeed

--- app/src/main/java/me/subhrajyoti/noteworthy/NotesViewModelFactory.kt
+++ app/src/main/java/me/subhrajyoti/noteworthy/NotesViewModelFactory.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import me.subhrajyoti.noteworthy.data.NotesRepository
 
+@Suppress("UNCHECKED_CAST")
 class NotesViewModelFactory (
     private val notesRepository: NotesRepository
 ) : ViewModelProvider.Factory {
